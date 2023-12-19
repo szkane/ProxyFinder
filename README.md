ec294220d3d3dd1b0b9d2124f45acc4ba15f3f9f
Kane <szkane@gmail.com>
2023-12-19 16:32:19 +0800

    alpha test

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ProxyFinder
 Find free proxy from public web and  check it available
+
+## 0x01 使用方法
+
+第一步：通过 [shodan.io](https://shodan.io) 或 [fofa.info](https://fofa.info/) 网站，获取你的目标IP。
+
+第二步：通过 Microsoft Edge 的Copilot 能力，把搜索结果的IP 输出成每行一个IP:PORT。
+
+第三步：将IP:PORT 复制到 proxies.txt 文件中
+
+第四步：在 iterm中 执行 *proxytest.sh* 测试通过的IP
+
+第五步: 可用代理IP出来了，就可以直接用咯
+
---
+++ proxytest.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# 代理列表文件路径
+PROXY_LIST='proxies.txt'
+
+# 成功的代理将被写入此文件
+WORKING_PROXIES='working_proxies.txt'
+
+# 测试的URL
+URL='http://www.gstatic.com/generate_204'
+
+# 清空或创建工作代理文件
+> "$WORKING_PROXIES"
+
+# 读取代理列表并进行测试
+while IFS= read -r proxy
+do
+    echo "Testing proxy: $proxy"
+    STATUS_CODE=$(curl -s -o /dev/null -w '%{http_code}' --proxy "$proxy" "$URL" --connect-timeout 3)
+    
+    if [ "$STATUS_CODE" -eq 204 ]; then
+        echo "Proxy $proxy is working."
+        echo "$proxy" >> "$WORKING_PROXIES"
+    else
+        echo "Proxy $proxy failed."
+    fi
+done < "$PROXY_LIST"
+
+# 统计工作代理文件中的行数
+WORKING_COUNT=$(wc -l < "$WORKING_PROXIES")
+
+echo "$WORKING_COUNT working proxies have been saved to $WORKING_PROXIES
